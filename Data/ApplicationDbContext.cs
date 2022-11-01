af11c48533009605a3e2dc1edebab3a200a566d7
Kristher Vidal <kristher@dripcoder.com>
2022-11-01 14:00:50 +0800

    WIP

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using PizzaPlayground.Models;
+
 namespace PizzaPlayground.Data;
 
-public class ApplicationDbContext
+public class ApplicationDbContext : DbContext
 {
-    
+    public DbSet<PizzaOrder> PizzaOrders { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base()
+    {
+        
+    }
 }
